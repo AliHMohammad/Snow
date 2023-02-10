8797cf9065de3684cf8ded750d8990c2ce2f729e
AliHMohammad <alihmohammad@hotmail.com>
2023-02-10 19:46:02 +0100

    Added random x-positions

--- snow.js
+++ snow.js
@@ -8,14 +8,8 @@ let flakes = document.querySelectorAll(".snowflake");
 init();
 
 function init() {
-  //Vi bestemmer værdien af browser-width
-  let maxWidth = document.querySelector("#nightsky").clientWidth;
 
   for (let i = 0; i < flakes.length; i++) {
-    //Vi bestemmer random værdi af x-værdi af objekt vha. maxWidth
-    let px = Math.random() * maxWidth;
-    flakes[i].style.left = px + "px";
-
     //Vi initierer reset(i) for at bestemme yPos, speed, scale af objekt OG ny værdi af xPos, Så den dukker et nyt sted op fra x-position.
     reset(i);
   }
@@ -25,14 +19,17 @@ function init() {
 }
 
 function reset(i) {
+  //Width af hjemmeside
   let maxWidth = document.querySelector("#nightsky").clientWidth;
-  xPos[i] = Math.random() * maxWidth;
   
+  //Vi bestemmer random værdi af x-værdi af objekt vha. maxWidth
+  xPos[i] = Math.random() * maxWidth;
+
   //Initering / ressetning af i position bestemmes med Math.random.
   //Minus så den starter udenfor browseren.
   yPos[i] = Math.random() * -1000;
   //Initering / ressetning af hastigheden for speed[i].
-  speed[i] = Math.random() * 2 + 1;
+  speed[i] = Math.random() * 1.7 + 0.6;
 
   //Initering / resettning af størrelsen af objektet
   flakes[i].style.transform = "scale(" + Math.random() + ")";
