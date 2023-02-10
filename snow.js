fad188de8a435536ca46cdde0c5d9b66f600539f
AliHMohammad <alihmohammad@hotmail.com>
2023-02-10 16:55:43 +0100

    Multiple falling snowflakes

--- snow.js
+++ snow.js
@@ -1,18 +1,37 @@
 let yPos = 0;
 
-move();
+init();
+
+function init() {
+    let flakes = document.querySelectorAll(".snowflake");
+    let px = 50
+
+    for (let i = 0; i < flakes.length; i++) {
+        flakes[i].style.left = px + "px"  
+        px = px + 50;
+    }
+    
+    //Starts moving snowflakes:
+    move();
+}
+
 
 function reset() {
     yPos = -10;
 
 }
 
-function move() {
-    document.querySelector(".snowflake").style.top = yPos + "px";
-    yPos++;
 
+function move() {
+    let flakes = document.querySelectorAll(".snowflake");
     let max = document.querySelector("#nightsky").clientHeight;
 
+    for (let i = 0; i < flakes.length; i++) {
+        flakes[i].style.top = yPos + "px"
+        
+    }
+    yPos++;
+    
     if (yPos > max) {
         reset();
     }
