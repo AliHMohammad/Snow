93b3aea863bc70f52bb32f78944a821ffc244b55
AliHMohammad <alihmohammad@hotmail.com>
2023-02-10 18:12:07 +0100

    Random speed and sizes

--- snow.js
+++ snow.js
@@ -1,15 +1,25 @@
 let yPos = []
+let speed = [];
+
+//Array af objekter
+let flakes = document.querySelectorAll(".snowflake");
 
 init();
 
 function init() {
-    let flakes = document.querySelectorAll(".snowflake");
-    let px = 50
+    
+    let px = 25
 
     for (let i = 0; i < flakes.length; i++) {
+        //Initiering af venstre position af objektet. Px inkrimineres med 50 hvert loop 
         flakes[i].style.left = px + "px";
+        px += 50;
+
+        //Initiering af top position af objektet med Math.random()
         yPos[i] = Math.random() * 100;
-        px = px + 50;
+        
+        //Initering af objekt hastighed med Math.random()
+        speed[i] = Math.random() * 10;
     }
     
     //Starts moving snowflakes:
@@ -18,26 +28,37 @@ function init() {
 
 
 function reset(i) {
+    //i position bestemmes med Math.random. Minus så den starter udenfor browseren.
     yPos[i] = Math.random() * -50;
+    //Hastigheden for speed[i] resettes med en ny.
+    speed[i] = Math.random() * 10;
+
+    //Resetter størrelsen af objektet
+    flakes[i].style.transform = "scale(" + Math.random() + ")";
+
 
 }
 
 
 function move() {
-    let flakes = document.querySelectorAll(".snowflake");
+    //browser-højde
     let max = document.querySelector("#nightsky").clientHeight;
 
     for (let i = 0; i < flakes.length; i++) {
-        flakes[i].style.top = yPos[i] + "px"
+      //Bevægelse
+      flakes[i].style.top = yPos[i] + "px";
+      
 
-        yPos[i]++;
-        
-        if (yPos[i] > max) {
-            reset(i);
-        }
-        
+      //Nuværende y-position adderes med objektets hastighed.
+      yPos[i] += speed[i];
+
+      //hvis y-position større end browser-højde, reset index "i" for den enkelte array
+      if (yPos[i] > max) {
+        reset(i);
+      }
     }
 
+    //Kør move() igen med et kort delay
     requestAnimationFrame(move);
 }
 
